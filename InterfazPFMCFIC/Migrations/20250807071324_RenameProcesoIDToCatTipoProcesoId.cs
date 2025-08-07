80c54e7a646f955def515e3ea8273f43bc976396
Ricardo Joel Garcia Garduño <lmpmx.2024@gmail.com>
2025-08-07 01:15:55 -0600

    se renombra la propiedad ProcesoID

---
+++ InterfazPFMCFIC/Migrations/20250807071324_RenameProcesoIDToCatTipoProcesoId.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InterfazPFMCFIC.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameProcesoIDToCatTipoProcesoId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProcesoID",
+                table: "INTERFAZ_PFM_CFIC_ARCHIVO",
+                newName: "CatTipoProcesoId"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "CatTipoProcesoId",
+                table: "INTERFAZ_PFM_CFIC_ARCHIVO",
+                newName: "ProcesoID"
+            );
+        }
+    }
+}
